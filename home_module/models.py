f0abefec04c74369d11f0b40210f3e2a0476d761
bahman pournazari <bahmanpn@gmail.com>
2024-11-17 22:20:11 +0330

    added modeltranslation package in project to add another fields for every field of models i want to translate+translation.py for home module app to set fields

--- home_module/models.py
+++ home_module/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title=models.CharField(max_length=127,null=True,verbose_name='product title')
+    price=models.PositiveIntegerField(default=0,verbose_name='product price')
+
+    def __str__(self):
+        return self.title
+
--- home_module/views.py
+++ home_module/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
+from .models import Product
 
 
 def index(request):
+    products = Product.objects.all()
     context = {
-        'data': _('hello world!!')
+        'data': _('hello world!!'),
+        'products': products
     }
 
     return render(request, 'home_module/index.html', context)
