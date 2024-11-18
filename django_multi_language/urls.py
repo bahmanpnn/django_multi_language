05f09ace85885b9af9940d6bcac3f03b62f414b2
bahman pournazari <bahmanpn@gmail.com>
2024-11-18 05:54:52 +0330

    added dropdown select option form to choose a language of site.2 views can handle language changing.first one that is main and send selected language in set_language of i18n module,but seconf way more customized and handle language selection in index view and form send data in index view again

--- django_multi_language/urls.py
+++ django_multi_language/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.i18n import i18n_patterns
+from django.views.i18n import set_language
 
-urlpatterns = []
+urlpatterns = [
+    path('set_language/', set_language, name='set_language'),
+]
 
 urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('home_module.urls')),
     path('rosetta/', include('rosetta.urls')),
-    prefix_default_language=False
+    prefix_default_language=True
     # if it is False it doesn't display default language in url==> # website.com/en/==>website.com
 )
--- home_module/views.py
+++ home_module/views.py
@@ -1,13 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils.translation import gettext_lazy as _
 from .models import Product
+from django.shortcuts import render, redirect
+from django.utils.translation import activate, get_language
+
+
+from django.shortcuts import render
+from django.utils.translation import gettext as _
 
 
 def index(request):
     products = Product.objects.all()
+
     context = {
         'data': _('hello world!!'),
-        'products': products
+        'products': products,
+        'selected_language': request.LANGUAGE_CODE,  # This gets the current language
     }
 
-    return render(request, 'home_module/index.html', context)
+    return render(request, 'home_module/index2.html', context)
+
+
+# ****it works too but before function of this, is cleaner
+
+# def index(request):
+#     # Get the selected language from the GET parameters
+#     selected_language = request.GET.get('language', None)
+#
+#     if selected_language:
+#         # Activate the selected language
+#         activate(selected_language)
+#         # request.session['LANGUAGE_SESSION_KEY'] = selected_language
+#         return redirect('index-page')  # Redirect to the home page to reflect the change
+#
+#     # Get the current language
+#     current_language = get_language()
+#
+#     products = Product.objects.all()
+#     return render(request, 'home_module/index2.html', {
+#         'data': _('hello world!!'),
+#         'products': products,
+#         'selected_language': current_language,  # Pass the current language to the template
+#     })
+
